4e0c61b961300b33e778cddaa7ed550cf1b2ae38
mrRonin <ronin.git@gmail.com>
2019-11-17 11:52:52 +0100

    visual ench

--- romodoro.py
+++ romodoro.py
@@ -9,7 +9,7 @@ from playsound import playsound
 print("""
            ,d                                               ,d
            88                                               88
-           MM88MMM ,adPPYba,  88,dPYba,,adPYba,  ,adPPYYba, MM88MMM ,adPPYba,
+         MM88MMM ,adPPYba,  88,dPYba,,adPYba,  ,adPPYYba, MM88MMM ,adPPYba,
            88   a8"     "8a 88P'   "88"    "8a ""     `Y8   88   a8"     "8a
            88   8b       d8 88      88      88 ,adPPPPP88   88   8b       d8
            88,  "8a,   ,a8" 88      88      88 88,    ,88   88,  "8a,   ,a8"
